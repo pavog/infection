484d224024e6089fb744a49e1286e463af433fa5
Paul Vogel <github@paulvogel.me>
2025-11-10 11:53:25 +0100

    add --build-arg PHP_VERSION=8.1 to build-docker.php

--- bin/ci/build-docker.php
+++ bin/ci/build-docker.php
@@ -50,4 +50,4 @@ $ref = escapeshellarg($ref);
 $composer_branch = escapeshellarg($composer_branch);
 $platform = escapeshellarg($platform);
 
-r("docker buildx build --push . -t ghcr.io/$user/infection:$ref-$platform --build-arg INFECTION_REV=$composer_branch -f devTools/Dockerfile");
+r("docker buildx build --push . -t ghcr.io/$user/infection:$ref-$platform --build-arg INFECTION_REV=$composer_branch --build-arg PHP_VERSION=8.1 -f devTools/Dockerfile");
